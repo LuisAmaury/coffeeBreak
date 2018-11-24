ba2759161cbe562311f9f0218914654e0b2fa613
LuisAmaury <luisamaury8@gmail.com>
2018-11-24 17:53:46 -0600

    total

--- resources/views/admin/sales/partials/form.blade.php
+++ resources/views/admin/sales/partials/form.blade.php
@@ -5,14 +5,14 @@
 	<div>
 	@foreach($products as $product)
 		<label>
-			{{ Form::checkbox('products[]', $product->id) }} {{ $product->name }}
+			{{ Form::checkbox('products[]', $product->id) }} {{ $product->name }} $ {{ $product->price}}
 		</label>
 	@endforeach
 	</div>
 </div>
 
 <div class="form-group">
-  {{ Form::number('total',50.00, ['class' => 'form-control', 'id' => 'total', 'hidden'=>true]) }}
+  {{ Form::number('total',null, ['class' => 'form-control', 'id' => 'total', 'hidden'=>true]) }}
 </div>
 
 <div class="form-group">
@@ -25,19 +25,35 @@
 $(document).ready(function(){
   var products = {!! json_encode($products->toArray()) !!};
   var cardNumber = '{!! auth()->user()->cardNumber !!} ';
-  console.log(total.value);
+  $('input[name="products[]"]').change(function(){
+        if ($(this).is(':checked')) {
+          var productsCheck = [];
+          var totalPrice = 0;
+          $.each($("input[name='products[]']:checked"), function(){
+                productsCheck.push($(this).val());
+            });
+          console.log(productsCheck);
+          console.log(products);
+          for(prod in productsCheck){
+            let id = parseInt(productsCheck[prod]) - 1;
+            totalPrice += products[id].price;
+            // console.log(productsCheck[id]);
+
+          }
+          }
+          total.value = totalPrice;
+    });
 });
+
 saveSale.addEventListener('click', function(){
   var cardNumber = '{!! auth()->user()->cardNumber !!} ';
-
-  console.log(total.value);
   var data = {
     'ctaorigen':  cardNumber,
     'ctadestino': '0000000000000010',
-    'monto': 50,
+    'monto': parseFloat(total.value),
     'detalle': 'venta de coffeeBreak'
   };
-  console.log(data);
+
   $.ajax({
     url: "https://spbank.herokuapp.com/api/payment",
     method: "POST",
